7396daf70d37712c3d938834653e4315f6d505e4
S00B1N3R <130852323+S00B1N3R@users.noreply.github.com>
2024-03-26 18:29:41 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function capture() {
+    Webcam.snap(function (data) {
+        document.getElementById("result").innerHTML = '<img id="selfie" src="' + data + '">'
+
+    })
+}
+function check() {
+
+}
+console.log("version", ml5.version)
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: "png",
+    png_quality: 90
+})
+camera = document.getElementById("camera")
+Webcam.attach("#camera")
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/gTWkjsCRr/model.json",modelloaded)
+function modelloaded(){
+    console.log("model is ready");
+}
