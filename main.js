89acab9e77f765d96f81dd8e9d92fd2cbd079fad
S00B1N3R <130852323+S00B1N3R@users.noreply.github.com>
2024-03-31 18:47:35 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ function capture() {
     })
 }
 function check() {
+    img = document.getElementById("selfie")
+    classifier.classify(img, gotresult)
 
 }
 console.log("version", ml5.version)
@@ -16,7 +18,16 @@ Webcam.set({
 })
 camera = document.getElementById("camera")
 Webcam.attach("#camera")
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/gTWkjsCRr/model.json",modelloaded)
-function modelloaded(){
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/gTWkjsCRr/model.json", modelloaded)
+function modelloaded() {
     console.log("model is ready");
+}
+function gotresult(error, result) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(result);
+        document.getElementById("resultObject").innerHTML = result[0].label
+        document.getElementById("resultAccuracy").innerHTML = result[0].confidence.toFixed(3)
+    }
 }
